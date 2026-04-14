b332f28fd98c213683600be6fe0209e51a181bbc
alisyed-sqa <ali@servicemarket.com>
2026-04-14 17:33:16 +0400

    added new class

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,12 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, Java");
+        System.out.println("New java project");
     }
+}
+
+class InnerHelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Java is fun");
+    }
+    
 }
