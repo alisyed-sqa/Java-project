43d9dbbd9752e6e3debc3f48e903f96f48480526
alisyed-sqa <ali@servicemarket.com>
2026-04-15 17:57:07 +0400

    1st arrat

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,14 +1,34 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("New java project");
+        InnerHelloWorld inner = new InnerHelloWorld();
+        inner.main();
     }
 }
 
 class InnerHelloWorld {
-    public static void main(String[] args) {
+    public void main() {
         System.out.println("Java is fun : testing");
+
+        int[] array = {500, 210, 330, 61, 87, 773, 81};
+        int max = array[0];
+        int min = array[0];
+
+        for (int i : array) {
+            if (i > max) {
+                max = i;
+            }
+        }
+
+        for (int i : array) {
+            if (i < min) {
+                min = i;
+            }
+        }
+
+        System.out.println("Max value: " + max);
+        System.out.println("Min value: " + min);
     }
-    
 }
 
 
