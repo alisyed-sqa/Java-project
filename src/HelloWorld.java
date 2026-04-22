1add329c64ac0db8c990644efa00f51581e99de8
alisyed-sqa <ali@servicemarket.com>
2026-04-22 17:37:46 +0400

    Revert "Java"
    This reverts commit a0460241cfb1dc16b44db0e5277b4b3f997410dd.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,3 @@
-// HelloWorld.java: Basic Java examples
-
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("New java project");
@@ -12,19 +10,16 @@ class InnerHelloWorld {
     public void main() {
         System.out.println("Java is fun : testing");
 
-        // Find max and min values in array
         int[] array = {500, 210, 330, 61, 87, 773, 81};
         int max = array[0];
         int min = array[0];
 
-        // Find maximum
         for (int i : array) {
             if (i > max) {
                 max = i;
             }
         }
 
-        // Find minimum
         for (int i : array) {
             if (i < min) {
                 min = i;
@@ -35,3 +30,6 @@ class InnerHelloWorld {
         System.out.println("Min value: " + min);
     }
 }
+
+
+// select * from HelloWorld;
